38a607d4209c160f0fdf4f1dcdae5aaaf7e8c53b
barok-el <barokshewangzaw123@gmail.com>
2026-06-06 10:25:51 +0300

    Add RequestLoggingMiddleware

--- TmsApi/Program.cs
+++ TmsApi/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication;
+using TmsApi.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,11 +10,14 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<RequestLoggingMiddleware>();
+app.UseExceptionHandler("/error");
+app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapGet("/api/assessments/results",()=> {
-    Console.WriteLine("MINIMAL API ENDPOINT CALLED");
+    //Console.WriteLine("MINIMAL API ENDPOINT CALLED");
     return Results.Ok(new
     {
     message = "Placeholder assessment results"
